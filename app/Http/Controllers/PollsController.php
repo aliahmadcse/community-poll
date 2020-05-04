3604360419d9c473f6689f5cedf0f8e3c8f4ffad
aliahmadcse <aliahmadcse@gmail.com>
2020-05-04 22:38:43 +0500

    Read a single record

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -9,6 +9,11 @@ class PollsController extends Controller
 {
     public function index()
     {
-        return response()->json(Poll::get(),200);
+        return response()->json(Poll::get(), 200);
+    }
+
+    public function show($id)
+    {
+        return response()->json(Poll::find($id), 200);
     }
 }
